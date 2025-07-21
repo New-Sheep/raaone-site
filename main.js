b7e68dae44df1ab1a0b6892b0adb97ac1944c1ec
New-Sheep <42493732+New-Sheep@users.noreply.github.com>
2025-07-21 14:52:28 +0100

    Add files via upload
    javascript epic

---
+++ main.js
@@ -0,0 +1,6 @@
+
+function endIntro() {
+    document.getElementById('intro').style.display = 'none';
+    document.getElementById('mainContent').style.display = 'block';
+    document.getElementById('introAudio').pause();
+}
